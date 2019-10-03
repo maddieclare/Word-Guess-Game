83018d3aefe2b1de24af5da52fb4b5762886bb12
Maddie <54539182+maddieclare@users.noreply.github.com>
2019-10-03 18:41:09 +0800

    Game fully functional but no CSS yet

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,15 +4,30 @@ var alphabet = "abcdefghijklmnopqrstuvwxyz";
 // all possible words to be guessed
 var listOfWords = [
   "halloween",
-  "ghost",
   "vampire",
   "werewolf",
-  "monster",
+  "omen",
   "haunted",
+  "graveyard",
+  "superstition",
+  "occult",
+  "skull",
+  "tomb",
+  "supernatural",
+  "phantom",
   "poltergeist",
-  "demon",
-  "possession",
-  "skeleton"
+  "broomstick",
+  "crypt",
+  "nightmare",
+  "cobwebs",
+  "demonic",
+  "skeleton",
+  "zombie",
+  "spectre",
+  "midnight",
+  "apparition",
+  "cemetery",
+  "cauldron"
 ];
 
 // these variables need declaring outside of their functions
@@ -32,15 +47,12 @@ var playerWins = 0;
 // variable tracking player losses
 var playerLosses = 0;
 
-// for variable logging in HTML
-function getStats() {
-  document.getElementById("playerWins").innerHTML = "Wins: " + playerWins;
-  document.getElementById("playerLosses").innerHTML = "Losses: " + playerLosses;
-  document.getElementById("wordToBeGuessed").innerHTML = "Current word: " + hiddenWord;
-  document.getElementById("numberOfGuessesLeft").innerHTML =
-    "Number of guesses remaining: " + numberOfGuessesLeft;
-  document.getElementById("lettersAlreadyGuessed").innerHTML =
-    "Your guesses so far: " + lettersAlreadyGuessed;
+// displays number of underscores corresponding with length of wordToBeGuessed
+function hideWord() {
+  for (var i = 0; i < wordToBeGuessed.length; i++) {
+    hiddenWord.push("_");
+    console.log(hiddenWord);
+  }
 }
 
 // selects item at random from listOfWords
@@ -49,53 +61,73 @@ function randomWordSelection() {
   console.log(wordToBeGuessed);
 }
 
-// displays number of underscores corresponding with length of wordToBeGuessed
-function hideWord() {
-    for (var i = 0; i < wordToBeGuessed.length; i++) {
-        hiddenWord.push("_");
-        console.log(hiddenWord);
-    }
-}
-
 // reveals correctly guessed letter
-function  showHiddenLetter() {
-    for (var i = 0; i < wordToBeGuessed.length; i++) {
-        if (wordToBeGuessed.charAt(i) == playerGuess) {
-            hiddenWord[i] = playerGuess;
-        }
+function showHiddenLetter() {
+  // where the number contained in variable (i) is less than the length of the word to bo guessed, (i) increases by 1 each loop
+  for (var i = 0; i < wordToBeGuessed.length; i++) {
+    // for each loop, if the character located at (i) in wordToBeGuessed is equal to the playerGuess...
+    if (wordToBeGuessed.charAt(i) == playerGuess) {
+      // ...the equivalent index in the hidden word array changes to match the letter guessed by the player
+      hiddenWord[i] = playerGuess;
+      console.log(hiddenWord);
     }
+  }
 }
 
 // prevents player from guessing the same letter twice
 function letterGuessedMoreThanOnce() {
   alert("You've already guessed that letter!");
+  console.log(numberOfGuessesLeft);
 }
 
 // prevents any non-alphabetical key from being counted as a guess
 function inputIsNotALetter() {
   alert("Please choose a valid letter.");
+  console.log(numberOfGuessesLeft);
 }
 
-// pushes valid player guesses to lettersAlreadyGuessed and decreases numberOfGuessesLeft by 1
+// pushes valid player guesses to lettersAlreadyGuessed
 function trackLettersGuessed() {
-  lettersAlreadyGuessed.push(" " + playerGuess);
-  numberOfGuessesLeft -= 1;
+  lettersAlreadyGuessed.push(playerGuess);
+  console.log(lettersAlreadyGuessed);
+}
+
+// decreases numberOfGuessesLeft by 1
+function decreaseNumberOfGuesses() {
+    numberOfGuessesLeft--;
 }
 
 // player lose condition
 function playerLoss() {
   alert("You lose :(");
   playerLosses += 1;
-  lettersAlreadyGuessed = [];
-  numberOfGuessesLeft = 9;
+  console.log(playerLosses);
 }
 
 // player win condition
 function playerWin() {
   alert("You win!");
   playerWins += 1;
+  console.log(playerWins);
+}
+
+// resets hiddenWord
+function reset() {
+  numberOfGuessesLeft = 12;
   lettersAlreadyGuessed = [];
-  numberOfGuessesLeft = 9;
+  hiddenWord = [];
+}
+
+// for variable logging in HTML
+function getStats() {
+    document.getElementById("playerWins").innerHTML = "Wins: " + playerWins;
+    document.getElementById("playerLosses").innerHTML = "Losses: " + playerLosses;
+    document.getElementById("wordToBeGuessed").innerHTML =
+      "Current word: " + hiddenWord;
+    document.getElementById("numberOfGuessesLeft").innerHTML =
+      "Number of guesses remaining: " + numberOfGuessesLeft;
+    document.getElementById("lettersAlreadyGuessed").innerHTML =
+      "Your guesses so far: " + lettersAlreadyGuessed;
 }
 
 randomWordSelection();
@@ -104,22 +136,32 @@ hideWord();
 
 getStats();
 
-// player can guess any letter of the alphabet by pressing the corresponding key
 document.onkeyup = function(selection) {
-  // key press adds letter to variable
   playerGuess = selection.key.toLowerCase();
 
-    if (alphabet.indexOf(playerGuess) == -1) {
+  if (alphabet.indexOf(playerGuess) == -1) {
     inputIsNotALetter();
   } else if (lettersAlreadyGuessed.indexOf(playerGuess) !== -1) {
     letterGuessedMoreThanOnce();
   } else if (wordToBeGuessed.indexOf(playerGuess) !== -1) {
     showHiddenLetter();
-  } else if (numberOfGuessesLeft == 1) {
-    playerLoss();
-    randomWordSelection();
+    trackLettersGuessed();
   } else {
     trackLettersGuessed();
+    decreaseNumberOfGuesses();
+  }
+ 
+
+  if (numberOfGuessesLeft == 0) {
+    playerLoss();
+    reset();
+    randomWordSelection();
+    hideWord();
+  } else if (hiddenWord.indexOf("_") == -1) {
+    playerWin();
+    reset();
+    randomWordSelection();
+    hideWord();
   }
 
   getStats();
