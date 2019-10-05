d22656079a9dd9ab74f04423bbbf15041d35c1f1
Maddie <54539182+maddieclare@users.noreply.github.com>
2019-10-05 19:27:26 +0800

    Added readme; removed console logging

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Word-Guess-Game
+
+## Instructions
+In this game, you will attempt to guess a randomly selected word using the alphabetical characters on your keyboard. 
+
+* There are a total of 12 guesses allowed
+* Correctly guessed letters will not count towards your total guesses
+* Non-alphabetical characters and letters guessed more than once will also not be counted
+* All words are halloween-themed
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -51,14 +51,12 @@ var playerLosses = 0;
 function hideWord() {
   for (var i = 0; i < wordToBeGuessed.length; i++) {
     hiddenWord.push("_");
-    console.log(hiddenWord);
   }
 }
 
 // selects item at random from listOfWords
 function randomWordSelection() {
   wordToBeGuessed = listOfWords[Math.floor(Math.random() * listOfWords.length)];
-  console.log(wordToBeGuessed);
 }
 
 // reveals correctly guessed letter
@@ -69,7 +67,6 @@ function showHiddenLetter() {
     if (wordToBeGuessed.charAt(i) == playerGuess) {
       // ...the equivalent index in the hidden word array changes to match the letter guessed by the player
       hiddenWord[i] = playerGuess;
-      console.log(hiddenWord);
     }
   }
 }
@@ -77,19 +74,16 @@ function showHiddenLetter() {
 // prevents player from guessing the same letter twice
 function letterGuessedMoreThanOnce() {
   alert("You've already guessed that letter!");
-  console.log(numberOfGuessesLeft);
 }
 
 // prevents any non-alphabetical key from being counted as a guess
 function inputIsNotALetter() {
   alert("Please choose a valid letter.");
-  console.log(numberOfGuessesLeft);
 }
 
 // pushes valid player guesses to lettersAlreadyGuessed
 function trackLettersGuessed() {
   lettersAlreadyGuessed.push(playerGuess);
-  console.log(lettersAlreadyGuessed);
 }
 
 // decreases numberOfGuessesLeft by 1
@@ -101,14 +95,12 @@ function decreaseNumberOfGuesses() {
 function playerLoss() {
   alert("You lose :(");
   playerLosses += 1;
-  console.log(playerLosses);
 }
 
 // player win condition
 function playerWin() {
   alert("You win!");
   playerWins += 1;
-  console.log(playerWins);
 }
 
 // resets hiddenWord
